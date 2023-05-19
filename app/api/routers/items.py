1d0af0bd48f4d50c56e694e4fdf00236d5da271d
kenneth-wangg <kenneth@plannerbee.co>
2023-05-20 01:00:45 +0800

    add todos

--- app/api/routers/items.py
+++ app/api/routers/items.py
@@ -12,13 +12,21 @@ service = ItemService(repository)
 @router.post("", status_code=201)
 def create_item(req: CreateItemRequest):
     item = service.create_item(req)
+
+    # TODO: Add response schema
+
     return {"id": item.id}
 
 
 @router.get("")
 def get_items(dt_from: str, dt_to: str):
     items = service.get_items_by_date_range(dt_from, dt_to)
+
+    # TODO: Move logic to service file
     total_price = sum(item.price for item in items)
+
+    # TODO: Add response schema
+
     response = {"items": items, "total_price": total_price}
     return response
 
@@ -26,6 +34,11 @@ def get_items(dt_from: str, dt_to: str):
 @router.get("/statistics")
 def get_items_by_category(category: str):
     items = service.get_items_by_category(category)
+
+    # TODO: Move logic to service file
     total_price = sum(item.price for item in items)
+
+    # TODO: Add response schema
+
     response = {"items": items, "total_price": total_price}
     return response
--- app/services/items.py
+++ app/services/items.py
@@ -13,6 +13,12 @@ class ItemService:
         self.repository = repository
 
     def create_item(self, req: CreateItemRequest):
+        # TODO:
+        # Given business logic: If re-insert an item with the same name,
+        # the record should be updated with the new price
+
+        # Add response schema
+
         now = datetime.now().strftime(DATETIME_STR_FORMAT)
         item = Item(
             **{
@@ -25,7 +31,9 @@ class ItemService:
         return self.repository.create_item(item)
 
     def get_items_by_date_range(self, dt_from: str, dt_to: str) -> List[Item]:
+        # TODO: Fix schema. Add response schema
         return self.repository.get_items_by_date_range(dt_from, dt_to)
 
     def get_items_by_category(self, category: str) -> List[Item]:
+        # TODO: Fix schema. Add response schema
         return self.repository.get_items_by_category(category)
