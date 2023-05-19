eb0eac8cc50cbcc453bb57b49f833c0f0ed32149
kenneth-wangg <kenneth@plannerbee.co>
2023-05-20 00:51:25 +0800

    allow local deployment of app with AWS Lambda and API Gateway with AWS SAM cli

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.api.routers.items import router as item_router
 
 app = FastAPI()
 
 app.include_router(item_router, prefix="/items")
+
+# Used for AWS Lambda deployment only
+lambda_handler = Mangum(app)
