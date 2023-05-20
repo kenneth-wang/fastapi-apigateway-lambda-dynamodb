3543e53deeacc6b5f17b387b21ba6f90b0969281
kenneth-wangg <kenneth@plannerbee.co>
2023-05-20 17:39:19 +0800

    remove duplicated code in tests

--- app/tests/test_item_service.py
+++ app/tests/test_item_service.py
@@ -3,8 +3,18 @@ from unittest.mock import MagicMock
 from app.models.items import CreateItemRequest, Item
 from app.services.items import ItemService
 
+item = Item(
+    id=1,
+    name="Item 1",
+    category="Stationary",
+    price=5.5,
+    created_dt="2022-01-01 10:00:00",
+    last_updated_dt="2022-01-01 10:00:00",
+)
+
 
 def test_insert_or_update_item_case_insert():
+    # test insert
     repository_mock = MagicMock()
     service = ItemService(repository_mock)
 
@@ -23,16 +33,7 @@ def test_insert_or_update_item_case_update():
 
     req = CreateItemRequest(name="Notebook", category="Stationary", price=5.5)
 
-    repository_mock.get_items_by_name.return_value = [
-        Item(
-            id=1,
-            name="Item 1",
-            category="Stationary",
-            price=5.5,
-            created_dt="2022-01-01 10:00:00",
-            last_updated_dt="2022-01-01 10:00:00",
-        )
-    ]
+    repository_mock.get_items_by_name.return_value = [item]
     service.insert_or_update_item(req)
     repository_mock.update_item.assert_called_once()
     repository_mock.get_items_by_name.assert_called_once()
@@ -42,20 +43,9 @@ def test_get_items_by_date_range():
     dt_from = "2022-01-01 10:00:00"
     dt_to = "2022-01-25 10:00:00"
 
-    items = [
-        Item(
-            id=1,
-            name="Item 1",
-            category="Stationary",
-            price=5.5,
-            created_dt="2022-01-01 10:00:00",
-            last_updated_dt="2022-01-01 10:00:00",
-        )
-    ]
-
     repository_mock = MagicMock()
     service = ItemService(repository_mock)
-    repository_mock.get_items_by_date_range.return_value = items
+    repository_mock.get_items_by_date_range.return_value = [item]
 
     result = service.get_items_by_date_range(dt_from, dt_to)
 
